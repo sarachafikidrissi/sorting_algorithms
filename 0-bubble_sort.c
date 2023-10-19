3fbd955c26e7ce1571dbd96e5129181e67f43980
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-10-18 17:54:18 -0700

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - A functio that sorts an array of integers in ascending order
+ * @array: the the array to be sorted
+ * @size: the size of array
+ * Return: Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, flag;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				flag = 1;
+			}
+
+		}
+		if (flag == 0)
+			break;
+	}
+}
+
--- sort.h
+++ sort.h
@@ -14,9 +14,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 
@@ -28,5 +28,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 #endif
