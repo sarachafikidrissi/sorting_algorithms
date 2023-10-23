ca1327226d735033691cdf0e0d459d71a1e7c964
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-10-23 12:09:40 -0700

    fixing memory leaks

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -13,7 +13,7 @@ void counting_sort(int *array, size_t size)
 	size_t i, max;
 	int *count_array, *output_array;
 
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
 
 	max = array[0];
@@ -27,28 +27,21 @@ void counting_sort(int *array, size_t size)
 	count_array = (int *)malloc(sizeof(int) * (max + 1));
 
 	for (i = 0; i <= max; i++)
-	{
 		count_array[i] = 0;
-	}
 	for (i = 0; i < size; i++)
-	{
 		count_array[array[i]]++;
-	}
 	for (i = 1; i <= max; i++)
-	{
 		count_array[i] = count_array[i] + count_array[i - 1];
-	}
 	print_array(count_array, max + 1);
 
 	output_array = malloc(sizeof(int) * size);
 
 	for (i = size - 1; i < size; i--)
-	{
 		output_array[--count_array[array[i]]] = array[i];
-	}
 	for (i = 0; i < size; i++)
 	{
 		array[i] = output_array[i];
 	}
 	free(count_array);
+	free(output_array);
 }
