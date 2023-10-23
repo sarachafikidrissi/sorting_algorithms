5717faa260766187cd40eeca69c8ff73478553c7
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-10-23 15:06:14 -0700

    merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include "sort.h"
 
+/**
+ * merge - A function that merges subarrays
+ * @array: Inputed array
+ * @output_array: Outputed array
+ * @lb: lower index
+ * @ub: higher index
+ * @mid: the middle of array
+ * Return: Nothing
+*/
 void merge(int *array, int lb, int mid, int ub, int *output_array)
 {
 	int i = lb, j = mid + 1, k = lb;
@@ -44,7 +53,14 @@ void merge(int *array, int lb, int mid, int ub, int *output_array)
 		array[k] = output_array[k];
 
 }
-
+/**
+ * merge_sort_ - A function that sorts subarrays
+ * @array: Inputed array
+ * @output_array: Outputed array
+ * @lb: lower index
+ * @ub: higher index
+ * Return: Nothing
+*/
 void merge_sort_(int *array, int lb, int ub, int *output_array)
 {
 	int mid;
@@ -55,11 +71,22 @@ void merge_sort_(int *array, int lb, int ub, int *output_array)
 
 		merge_sort_(array, lb, mid, output_array);
 		merge_sort_(array, mid + 1, ub, output_array);
+		printf("Merging...\n[left]: ");
+		print_array(array + lb, mid - lb + 1);
+		printf("[right]: ");
+		print_array(array + mid + 1, ub - mid);
 		merge(array, lb, mid, ub, output_array);
+		printf("[Done]: ");
+		print_array(array + lb, ub - lb + 1);
 	}
 }
 
-
+/**
+ * merge_sort - A function that sorts an array
+ * @array: Inputed array
+ * @size: nomber of elements
+ * Return: Nothing
+*/
 void merge_sort(int *array, size_t size)
 {
 	int lb, ub;
